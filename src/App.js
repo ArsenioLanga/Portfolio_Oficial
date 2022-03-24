b7d57d4ee28b80ad8ebcad405bcdf716828f43ae
ArsenioLanga <arseniosergiolanga@gmai.com>
2022-03-24 13:44:21 +0200

    Extrutura da seccao fala-comigo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Home from "./PortfolioContainer/Home/Home";
 import About from "./PortfolioContainer/About/About";
 import Resume from "./PortfolioContainer/Resume/Resume";
+import Contact from "./PortfolioContainer/Contact/Contact";
 function App(){
   return(
     <div className='App'>
        <Home />
        <About />
        <Resume />
+       <Contact />
     </div>
   )
 }
---
+++ src/PortfolioContainer/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Contact.css";
+
+
+export default function Resume(){
+    return(
+        <div className="conctact-container">
+
+        </div>
+    )
+}
